ea8de0a9155d65d779a2efa1f429a436753373ef
vinsonf <vfernandez@shift3tech.com>
2021-12-07 09:52:13 -0800

    refactor(server, schemas): delete unused schemas

--- src/server/schemas/position.schema.ts
+++
@@ -1,9 +0,0 @@
-import mongoose from 'mongoose';
-const {Schema, model} = mongoose;
-
-const positionSchema = new Schema<any>({
-   position: String,
-   card: { type: Schema.Types.ObjectId, ref: 'Card' },
-});
-
-export const PositionModel = model('Center', positionSchema);
